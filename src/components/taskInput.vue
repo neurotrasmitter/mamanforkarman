92471ea38e515f651e399b2c6500b7c1565f944c
Your Name <you@example.com>
2022-01-27 17:35:33 +0300

    first commit

---
+++ src/components/taskInput.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "taskInput"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Family.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Family"
+    }
+</script>
+
+<style scoped>
+
+</style>
